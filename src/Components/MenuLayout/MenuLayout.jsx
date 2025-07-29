d524f9dc96f29a0e97c2931ca8f706569bd4616c
MohdRaza710 <mrmraza.710@gmail.com>
2025-07-29 20:49:32 +0300

    Update MenuLayout.jsx

--- src/Components/MenuLayout/MenuLayout.jsx
+++ src/Components/MenuLayout/MenuLayout.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header'; 
-import { allPaths } from '../../utils/constants'; 
+import { allPaths } from '../../Utils/constants'; 
 // setCollapsed import removed as it's no longer needed
 import { setShowAlgoPage } from '../../Redux/actions/generalActions'; 
 import { 
